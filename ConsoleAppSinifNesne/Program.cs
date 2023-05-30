b1e5953d39a7363f2f014c8ed00f7c33a6019f67
Korayckr <koraycakir1@hotmail.com>
2023-05-30 10:30:13 +0300

    Add project files.

---
+++ ConsoleAppSinifNesne/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleAppSinifNesne
+{
+
+    public class arac
+    {
+        public string yakit;
+        public int hiz;
+        public string renk;
+        public string marka;
+
+        public void aracInfo()
+        {
+            string tasit = "Taşıtın Markası: " + marka + "Aracın Rengi: " +
+                renk + "Aracın Yakıt Türü: " + yakit + "Aracın Hızı: " + hiz;
+
+            Console.WriteLine(tasit);
+
+        }
+
+    }
+
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            arac otomobil = new arac();
+
+            otomobil.hiz = 220;
+            otomobil.yakit = "Dizel \n";
+            otomobil.renk = "Kırmızı \n";
+            otomobil.marka = "Opel \n";
+
+
+            otomobil.aracInfo();
+                }
+    }
+
+
+
+
+    
+
+
+}
